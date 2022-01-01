f94fcfbb677da871ac48316e6e90517db7a78c5f
Huang Ke <328750202@qq.com>
2022-01-01 22:51:11 +0800

    加一些注释帮助理清代码思路，修了一个bug

--- src/main/java/huangke/lessonselectionsystem/viewandcontroller/Main.java
+++ src/main/java/huangke/lessonselectionsystem/viewandcontroller/Main.java
@@ -7,8 +7,6 @@ import huangke.lessonselectionsystem.model.database.DatabaseConnection;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
@@ -33,6 +31,7 @@ public class Main {
             (byte) 0, "张三", (byte) 0, (byte) 1, "A101"
     );
 
+    // 设置了一个函数叫做defaultFrame,统一JFrame的长和宽的大小。
     private static JFrame defaultFrame(String title) {
         JFrame frame = new JFrame(title);
         frame.setSize(WIDTH, HEIGHT);
@@ -40,40 +39,44 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        JFrame frame = defaultFrame("选课系统");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = defaultFrame("选课系统"); // 创建窗口”选课系统“
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 关闭窗口后整个程序退出
 
-        frame.setLayout(new GridLayout(0, 1));
+        frame.setLayout(new GridLayout(0, 1)); // 设置为GridLayout格式（网格布局）
 
-        JButton studentButton = new JButton("学生端");
-        studentButton.addActionListener(new ActionListener() {
+        JButton studentButton = new JButton("学生端"); // 创建按钮”学生端“
+        studentButton.addActionListener(e -> showStudentLoginFrame());
+        // 在点击studentButton按钮时执行shouStudentLoginFrame函数
+        // 这个写法叫做lambda表达式，作用是把->后面的函数作为参数传给这个函数。在这里，传进来的函数是在每次鼠标点击按钮时被执行。
+        /*studentButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 showStudentLoginFrame();
             }
-        });
-        frame.add(studentButton);
+        });*/
+        frame.add(studentButton); // 把按钮添加到窗口中
         JButton administratorButton = new JButton("管理员端");
         frame.add(administratorButton);
 
-        frame.setVisible(true);
+        frame.setVisible(true); // 设置窗口为可见
 
 
     }
 
+    // 学生端登录界面
     private static void showStudentLoginFrame() {
         JFrame frame = defaultFrame("登录");
         frame.setLayout(new GridLayout(0, 1));
 
-        // numberPanel
-        JPanel numberPanel = new JPanel(new GridLayout(1, 0));
-        JLabel numberLabel = new JLabel("学号：");
-        JTextField numberTextField = new JTextField();
+        // 学号面板
+        JPanel numberPanel = new JPanel(new GridLayout(1, 0)); // 设置面板
+        JLabel numberLabel = new JLabel("学号："); // 设置标签“学号"
+        JTextField numberTextField = new JTextField(); // 设置输入框
         numberPanel.add(numberLabel);
         numberPanel.add(numberTextField);
         frame.add(numberPanel);
 
-        // passwordPanel
+        // 密码面板
         JPanel passwordPanel = new JPanel(new GridLayout(1, 0));
         JLabel passwordLabel = new JLabel("密码：");
         JTextField passwordField = new JPasswordField();
@@ -83,29 +86,26 @@ public class Main {
 
         // loginButton
         JButton loginButton = new JButton("登录");
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Student student = databaseConnection.queryStudent(numberTextField.getText());
+        loginButton.addActionListener(e -> {
+            try {
+                Student student = databaseConnection.queryStudent(numberTextField.getText());
 
-                    if (student != null)
-                        if (passwordField.getText().equals(student.getPassword())) {
-                            frame.dispose();
-                            showCourseSelectionFrame();
-                        } else
-                            new JDialog(frame, "密码错误！")
-                                    .setVisible(true);
-                    else
-                        new JDialog(frame, "学号不存在！")
+                if (student != null)
+                    if (passwordField.getText().equals(student.getPassword())) {
+                        frame.dispose(); // 关掉窗口
+                        showCourseSelectionFrame(); // 选课界面
+                    } else
+                        new JDialog(frame, "密码错误！")
                                 .setVisible(true);
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                    new JDialog(frame, "发生未知 SQL 错误！")
+                else
+                    new JDialog(frame, "学号不存在！")
                             .setVisible(true);
-                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                new JDialog(frame, "发生未知 SQL 错误！")
+                        .setVisible(true);
             }
-        });
+        }); // 同上此处为lambda表达式，作用是把->后面的函数作为参数传给这个函数。在这里，传进来的函数是在每次鼠标点击按钮时被执行。
         frame.add(loginButton);
 
         frame.setVisible(true);
@@ -114,13 +114,13 @@ public class Main {
     private static void showCourseSelectionFrame() {
         JFrame frame = defaultFrame("学生选课");
 
-        JTabbedPane tabbedPane = new JTabbedPane();
+        JTabbedPane tabbedPane = new JTabbedPane(); // 设置标签页
 
-        Object[] columnNames = new Object[]{"课程ID", "课程名", "课序号", "教师", "第几节课", "教室"};
+        Object[] columnNames = new Object[]{"课程ID", "课程名", "课序号", "教师", "第几节课", "教室"}; // 设置一维数组,含有这些信息，对应数据库里的表头
         Object[][] data = new Object[][]{
-                Utils.lessonToJTableObjectArray(EXAMPLE_LESSON)
+                Utils.lessonToJTableObjectArray(EXAMPLE_LESSON) // 把Lesson对象转换成Object数组
         };
-        JTable table = new JTable(data, columnNames);
+        JTable table = new JTable(data, columnNames); // 建立了一个表格，横着的为data，竖着的为columnNames
         /*table.setModel(new DefaultTableModel() {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -132,7 +132,7 @@ public class Main {
             public void mouseClicked(MouseEvent e) {
                 Point point = e.getPoint();
                 int row = table.rowAtPoint(point);
-                int column = table.rowAtPoint(point);
+                int column = table.columnAtPoint(point);
 
                 showConfirmDialog(frame, EXAMPLE_LESSON);
             }
@@ -144,8 +144,9 @@ public class Main {
         frame.setVisible(true);
     }
 
+    // 确认选课界面
     private static void showConfirmDialog(Frame owner, Lesson lesson) {
-        JDialog dialog = new JDialog(owner, "确认选课吗？");
+        JDialog dialog = new JDialog(owner, "确认选课吗？"); // 弹出对话框“确认选课吗？”
         dialog.setLayout(new GridLayout(0, 1));
 
         dialog.setSize(DIALOG_WIDTH, DIALOG_HEIGHT);
@@ -155,11 +156,8 @@ public class Main {
         dialog.add(textArea);
 
         JButton button = new JButton("确认选课");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-            }
+        button.addActionListener(e -> {
+            // TODO
         });
         dialog.add(button);
 
